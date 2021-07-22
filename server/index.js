4d650de69c2a86db57461c647f032858d7eb2369
Matias Walter <matiwalter97@hotmail.com>
2021-07-22 19:03:24 -0300

    add api routes

--- server/index.js
+++ server/index.js
@@ -13,6 +13,59 @@ app.get('/', (req, res) => {
   res.send('Backend online');
 });
 
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    
+    const token = await axios.post('http://challenge-react.alkemy.org', {
+      email,
+      password
+    });
+    res.status(200).send(token);
+
+  } catch (error) {
+
+    res.status(400).json({
+      ok: false,
+      msg: 'Please contact the website administrator'
+    });
+  }
+});
+
+app.get('/hero/:id', async (req, res) => {
+  try {
+
+    const { id } = req.params;
+    const { data } = await axios.get(`https://superheroapi.com/api/${ACCESS_TOKEN}/${id}`);
+    res.status(200).send(data);
+
+  } catch (error) {
+
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Please contact the website administrator'
+    });
+  }
+});
+
+app.get('/search/:name', async (req, res) => {
+  try {
+
+    const { name } = req.params;
+    const { data: { results } } = await axios.get(`https://superheroapi.com/api/${ACCESS_TOKEN}/search/${name}`);
+    res.status(200).send(results);
+
+  } catch (error) {
+
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Please contact the website administrator'
+    });
+  }
+});
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
