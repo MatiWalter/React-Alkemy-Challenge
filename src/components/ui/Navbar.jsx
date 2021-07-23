9e49b1fb65a1c6ea5a9dd3f94dc413ee85074d62
Matias Walter <matiwalter97@hotmail.com>
2021-07-23 03:04:58 -0300

    add Search Screen

---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,77 @@
+import React, { useContext } from 'react';
+import { Link, NavLink, useHistory } from 'react-router-dom';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
+
+export const Navbar = () => {
+
+  const { dispatch } = useContext(AuthContext);
+  const history = useHistory();
+
+  const handleLogout = () => {
+    history.replace('/login');
+
+    dispatch({
+      type: types.logout
+    });
+  }
+
+  return (
+    <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link
+          className="navbar-brand"
+          to="/"
+        >
+          SuperheroesApp
+        </Link>
+        <button
+          type="button"
+          className="navbar-toggler"
+          data-bs-toggle="collapse"
+          data-bs-target="#navCollapse"
+          aria-controls="navCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div className="collapse navbar-collapse" id="navCollapse">
+          <div className="navbar-nav me-auto mb-2 mb-lg-0">
+
+            <NavLink
+              activeClassName="active"
+              className="nav-item nav-link"
+              exact
+              to="/team"
+            >
+              Team
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              className="nav-item nav-link"
+              exact
+              to="/search"
+            >
+              Search
+            </NavLink>
+          </div>
+
+          <div className="navbar-nav">
+            <ul className="navbar-nav me-auto">
+              <button
+                className="nav-item nav-link btn p-0"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
