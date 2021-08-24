5f14fe8c7af8176cf15949d3c61e90342be1e247
Matias Walter <matiwalter97@hotmail.com>
2021-08-24 20:20:39 -0300

    add deploy config

--- server/api/index.js
+++ server/api/index.js
@@ -46,3 +46,9 @@ app.get('/search/:name', async (req, res) => {
     });
   }
 });
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
--- src/helpers/getHeroes.js
+++ src/helpers/getHeroes.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getHeroesByName = async (name) => {
   try {
-    const url = `http://localhost:8080/search/${name}`;
+    const url = `https://mw-superhero-back.herokuapp.com/search/${name}`;
     const { data: heroes } = await axios.get(url);
     return heroes;
   } catch (error) {
@@ -11,7 +11,7 @@ export const getHeroesByName = async (name) => {
 }
 
 export const getHeroById = async (heroId) => {
-  const url = `http://localhost:8080/hero/${heroId}`;
+  const url = `https://mw-superhero-back.herokuapp.com/hero/${heroId}`;
   const { data: hero } = await axios.get(url);
   return hero;
 }
